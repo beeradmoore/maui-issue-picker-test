594ab51fcb95cdd0ce900e6945545a38d854ec5e
Brad <brad@4pi.com.au>
2023-12-12 14:16:16 +1100

    Initial commit

---
+++ PickerTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PickerTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ PickerTest/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PickerTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+
